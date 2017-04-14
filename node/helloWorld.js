68baa63de247394d3507874610968f2eefe58ec5
hyunsu SIM <simhs5082@gmail.com>
2017-04-14 11:13:07 +0900

    node server & route test

--- node/helloWorld.js
+++ node/helloWorld.js
@@ -1,8 +1,40 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
+
+
+function serveStaticFile(res, path, contentType, responseCode) {
+  if(!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, function (err, data) {
+      console.log(__dirname);
+      console.log(path);
+      console.log(data);
+     if(err){
+         res.writeHead(500, { 'Content-Type': 'text/plain'});
+         res.end('500 - Internal Error');
+     } else{
+         res.writeHead(responseCode,
+             { 'Content-Type': contentType });
+         res.end(data);
+     }
+  });
+};
 
 http.createServer(function (req,res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain'});
-    res.end('Hello World');
+    var path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase(); // url에서 쿼리스트링과 옵션인 마지막 슬래시를 지우고 소문자로 바꿔서 정규화함. ( ex - http://localhost:3000/?foo=bar -> http://localhost:3000 )
+    switch(path) {
+        case '':
+            serveStaticFile(res, '/public/home.html','text/html');
+            break;
+        case '/about':
+            serveStaticFile(res, '/public/about.html','text/html');
+            break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg','image/jpeg');
+            break;
+        default:
+            serveStaticFile(res, '/public/404.html','text/html',404);
+            break;
+    }
 }).listen(3000);
 
-console.log('Server started on local host:3000; press Ctrl - C to termiante')
+console.log('Server started on local host:3000; press Ctrl - C to termiante');
