8b1b321bcfb3a08bc80e3fbac8f8a2464a81d462
hyunsu SIM <hyunsu.sim@ysmcorp.com>
2017-04-26 15:58:27 +0900

    cookie & session update

--- node/app.js
+++ node/app.js
@@ -14,7 +14,18 @@ var hbs = require('express-handlebars').create({
 var fortune = require('./lib/fortune');
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
+var credentials = require('./credentials');
 
+var  VALID_EMAIL_REGEX = new RegExp(
+    '^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~=]+@' + '[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?' + '(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$'
+);
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,  // 요청이 바뀌지 않았어도 세션 정보를 강제로 다시 저장
+    saveUninitialized: false, // true - 새로운(초기화되지 않은) 세션도 저장, false - 쿠키를 설정하기 전에 사용자의 허락을 받아야한다.
+    secret: credentials.cookieSecret  // 세션 ID쿠키에 서명할때 사용하는 키
+}));
 app.use('/upload', function(req, res, next){
    var now = Date.now();
    jqupload.fileHandler({
@@ -34,16 +45,27 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
+    //console.log("res.cookie('monster','nom nom') : ",res.cookie('monster','nom nom'));
+    req.session.username = 'Anonymous';
+    var colorScheme = req.session.colorScheme || 'dark';
+    req.session.username = null;
+    delete req.session.colorScheme;
+
+    res.cookie('monster','nom nom');
+    res.cookie('signed_monster','nom nom',{ signed: true, path: '/foo' });
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     if(!res.locals.partials) res.locals.partials = {};
     res.locals.partials.weatherContext = getWeatherData();
+
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
 
     next();
 });
 
 app.use(require('body-parser').urlencoded({extended: true}));
 app.get('/newsletter',function (req, res) {
-   res.render('newsletter', { csrf: 'CSRF token goes here'});
+    res.render('newsletter', { csrf: 'CSRF token goes here'});
 });
 
 app.post('/process',function (req, res) {
@@ -52,10 +74,45 @@ app.post('/process',function (req, res) {
     // console.log('Name (from visible form field): ', req.body.name);
     // console.log('Email (from visible form field): ', req.body.email);
     // console.log('req : ',req)
-    console.log('req.xhr : ',req.xhr);
-    console.log('req.accepts("json,html") : ',req.accepts('json,html'));
     if(req.xhr || req.accepts('json,html') === 'json') res.send({ success: true });
     else res.redirect(303, '/thank-you'); // 에러가있다면  에러페이지로 리다이렉트
+});
+
+app.post('/newsletter', function(req, res){
+    var name = req.body.name || '',
+        email = req.body.email || '';
+
+    if(!email.match(VALID_EMAIL_REGEX)){
+        console.log('aaa');
+        if(req.xhr) return res.json({ error: 'Invalid name email address.'});
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid',
+        };
+        console.log(req.session.flash);
+        return res.redirect(303, '/newsletter/archive');
+    }
+
+    // new NewsletterSignup({ name: name, email: email}).save(function (err) {
+    //     console.log('b');
+    //     if(err) {
+    //         if(req.xhr) return res.json({error: 'Database error'});
+    //         req.session.flash = {
+    //             type: 'danger',
+    //             intro: 'Database error!',
+    //             message: 'There was a database error; please try again later.',
+    //         };
+    //         return res.redirect(303,'/newsletter/archive');
+    //     }
+    //     if(req.xhr) return res.json({ success: true});
+    //     req.session.flash = {
+    //         type: 'success',
+    //         intro: "Thank you!",
+    //         message: 'You have now been signed up for the newsletter.',
+    //     };
+    //     return res.redirect(303, 'newsletter/archive');
+    // });
 });
 
 app.disable('x-powered-by');
